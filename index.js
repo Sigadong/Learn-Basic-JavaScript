b2e0f5a9687b48a84fae8628c2b0d5c4fbded90c
Sigadong <irwanto.regar30@gmail.com>
2021-04-07 23:17:57 +0700

    Error Handling : try-catch-finally.

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 // # ERROR HANDLING
 
-/* Error Handling.
-Ketika mengembangkan sebuah aplikasi, sebagus apa pun kita membuatnya, akan selalu ada kemungkinan terjadi error atau crash. Entah terjadi karena kesalahan kita dalam menyusun logika kode, kesalahan input dari user, atau banyak alasan lainnya.
-Ketika aplikasi mengalami eror, aplikasi akan berhenti dan program setelahnya tidak akan dieksekusi.
-
-Sebagai developer, tentunya menjadi tugas kita untuk memastikan aplikasi tetap berjalan bagaimana pun kondisinya. Termasuk mengatasi ketika terjadi eror. Lantas bagaimana?
-
-Pada modul ini, kita akan mempelajari beberapa hal, seperti:
- - Bagaimana menangani error yang mungkin muncul pada aplikasi supaya tidak crash.
- - Membuat custom error untuk menandai error yang tidak tersedia pada JavaScript.
+/* try-catch-finally.
+Selain try dan catch, ada satu blok lagi yang ada dalam mekanisme error handling pada JavaScript, yaitu finally. Blok finally akan tetap dijalankan tanpa peduli apa pun hasil yang terjadi pada blok try-catch.
 */
 
 try {
-  console.log("Awal blok try");   // (1)
-  errorCode;                      // (2)
-  console.log("Akhir blok try");  // (3)
+  console.log("Awal blok try");
+  console.log("Akhir blok try");
 } catch (error) {
-  console.log(error.name);
-  console.log(error.message);
-  console.log(error.stack);
+  console.log("Baris ini diabaikan");
+} finally {
+  console.log("Akan tetap dieksekusi");
 }
