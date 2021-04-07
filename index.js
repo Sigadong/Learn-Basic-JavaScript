37c6f217a2e12621898a41734e6d4aa265725fdc
Sigadong <irwanto.regar30@gmail.com>
2021-04-07 12:56:14 +0700

    Higher-Order Function.

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 // # FUNCTIONAL PROGRAMMING
 
 /*
-=> Immutability. 
-Immutable berarti sebuah objek tidak boleh diubah setelah objek tersebut dibuat.
-Kontras dengan mutable yang artinya objek boleh diubah setelah objek tersebut dibuat.
+=> Higher-Order Function.
+JavaScript memiliki kemampuan First Class Functions, karena itu fungsi pada JavaScript dapat diperlakukan layaknya sebuah data. Kita bisa menyimpan function dalam variabel, memberikan function sebagai parameter pada fungsi lainnya, hingga mengembalikan function di dalam function.
 */
 
-const user = {
-  firstname: 'Harry',
-  lastName: 'Protter', // ups, typo!
+const hello = () => {
+  console.log('Hello!')
+};
+
+const say = (someFunction) => {
+  someFunction();
 }
 
-const createUserWithNewLastName = (newLastName, user) => {
-  return { ...user, lastName: newLastName }
+const sayHello = () => {
+  return () => {
+    console.log('Hello!');
+  }
 }
 
-const newUser = createUserWithNewLastName('Potter', user);
+hello();
+say(hello);
+sayHello()();
 
-console.log(newUser);
+
+/*
+Higher-Order Function menjadi bagian konsep pada paradigma FP. Higher-Order Function merupakan fungsi yang dapat menerima fungsi lainnya pada argumen; mengembalikan sebuah fungsi; atau bahkan keduanya.
+
+Teknik Higher-Order Function biasanya digunakan untuk:
+
+Mengabstraksi atau mengisolasi sebuah aksi, event, atau menangani alur asynchronous menggunakan callback, promise, dan lainnya.
+Membuat utilities yang dapat digunakan diberbagai tipe data.
+Membuat teknik currying atau function composition.
+
+
+Array map() merupakan salah satu contoh Higher-Order Function yang ada di JavaScript. Karena dalam penggunaanya, ia menerima satu buah argumen yang merupakan sebuah function.
+
+Dengan mengetahui adanya Higher-Order Function, Anda bisa membuat fungsi map() versi Anda sendiri!
+*/
+
+
+const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
+
+const arrayMap = (array, fn) => {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(fn(array[i]));
+  }
+
+  return newArray;
+}
+
+const newNames = arrayMap(names, (name) => `${name}!`);
+
+console.log({
+  names,
+  newNames,
+});
