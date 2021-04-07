93e59ff7eca7a2ae2e6240a0c4d6c4829cd329c0
Sigadong <irwanto.regar30@gmail.com>
2021-04-08 00:20:59 +0700

    Error Handling: Throwing Errors >< Custom Error (Extending an Error).

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // # ERROR HANDLING
 
-/* Throwing Errors.
-throw.
-Dengan if statement.
+/* Custom Error (Extending an Error).
+Ketika mengembangkan suatu aplikasi, akan ada banyak sekali kemungkinan munculnya eror. Seringkali, kita membutuhkan kelas eror sendiri untuk menunjukkan kesalahan yang spesifik dan tidak tersedia dalam kelas Error bawaan dari JavaScript.
 */
 
 let json = '{ "name": "Yoda", "age": 20 }';
@@ -29,5 +28,14 @@ try {
 }
 
 /*
-Dengan operator instanceOf, kita bisa mendapatkan tipe dari eror yang terjadi. Dari sana kita bisa membuat percabangan bagaimana cara menangani erornya.
+Awalnya, JSON.parse akan mengonversi data String menjadi object. Apabila format String tidak sesuai, maka fungsi tersebut akan melemparkan SyntaxError. Meskipun format atau sintaksis dari json string sudah sesuai, tetap ada kemungkinan data di dalamnya tidak lengkap. Saat ini kita masih menggunakan SyntaxError untuk menandai eror akibat data yang tidak lengkap, padahal secara sintaksis tidak ada masalah dari variabel json. Tentunya akan lebih baik jika kita punya Error yang lebih spesifik, bukan?
+
+Untuk itu kita bisa membuat kelas Error kita sendiri dengan nama dan pesan yang lebih sesuai. Kelas ini merupakan turunan dari kelas Error yang sudah ada. Sebagai contoh, untuk mengecek validasi data dari json, kita bisa membuat kelas Error seperti ini:
+
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError";
+    }
+}
 */
