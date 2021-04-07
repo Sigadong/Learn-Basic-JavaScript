70146e3efe947dff6d5e2a47791b4545faed4499
Sigadong <irwanto.regar30@gmail.com>
2021-04-07 23:16:14 +0700

    Error Handling.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// # MODULE
+// # ERROR HANDLING
 
-// ES6 Module
+/* Error Handling.
+Ketika mengembangkan sebuah aplikasi, sebagus apa pun kita membuatnya, akan selalu ada kemungkinan terjadi error atau crash. Entah terjadi karena kesalahan kita dalam menyusun logika kode, kesalahan input dari user, atau banyak alasan lainnya.
+Ketika aplikasi mengalami eror, aplikasi akan berhenti dan program setelahnya tidak akan dieksekusi.
 
-import coffeeStock from './state.js';
+Sebagai developer, tentunya menjadi tugas kita untuk memastikan aplikasi tetap berjalan bagaimana pun kondisinya. Termasuk mengatasi ketika terjadi eror. Lantas bagaimana?
 
-const displayStock = stock => {
-  for (const type in stock) {
-    console.log(type);
-  }
+Pada modul ini, kita akan mempelajari beberapa hal, seperti:
+ - Bagaimana menangani error yang mungkin muncul pada aplikasi supaya tidak crash.
+ - Membuat custom error untuk menandai error yang tidak tersedia pada JavaScript.
+*/
+
+try {
+  console.log("Awal blok try");   // (1)
+  errorCode;                      // (2)
+  console.log("Akhir blok try");  // (3)
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+  console.log(error.stack);
 }
-
-displayStock(coffeeStock);
