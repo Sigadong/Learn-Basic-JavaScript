5ab243178b26405f0a3bcee16b06422bfc1dd3b9
Sigadong <irwanto.regar30@gmail.com>
2021-04-07 23:32:53 +0700

    Error Handling: Throwing Errors -> undefined = error code.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // # ERROR HANDLING
 
 /* Throwing Errors.
-Lalu, bagaimana jika json string tidak sesuai dengan format object JavaScript?
+Lalu, bagaimana jika json yang didapat adalah seperti ini?
 
-Apabila json tidak sesuai format, maka JSON.parse akan menimbulkan eror. Eror tersebut akan ditangkap oleh blok catch dan kode di dalamnya yang akan dieksekusi.
 */
 
-let json = '{ bad json }';
+let json = '{ "age": 20 }';
 
 try {
   let user = JSON.parse(json);
@@ -17,3 +16,9 @@ try {
   console.log(error.name);
   console.log(error.message);
 }
+
+/*
+Secara sintaksis, kode di atas tidak menimbulkan eror, sehingga blok catch akan diabaikan. Namun, tidak adanya properti name pada json sebenarnya sama saja dengan eror karena akan berdampak pada jalannya program kita.
+
+Untuk mengatasinya, kita bisa menggunakan throw
+*/
