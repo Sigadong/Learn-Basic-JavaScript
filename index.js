e4886c3d433089d916c0e82e91a9970bd59afdd3
Sigadong <irwanto.regar30@gmail.com>
2021-04-07 23:40:22 +0700

    Error Handling: Throwing Errors -> SyntaxError = error code.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // # ERROR HANDLING
 
 /* Throwing Errors.
-Lalu, bagaimana jika json yang didapat adalah seperti ini?
-
+throw.
+Operator ini akan “melemparkan” eror pada program, sehingga eksekusi kode akan masuk pada blok catch.
 */
 
 let json = '{ "age": 20 }';
@@ -10,15 +10,16 @@ let json = '{ "age": 20 }';
 try {
   let user = JSON.parse(json);
 
-  console.log(user.name);
-  console.log(user.age);
+  if (!user.name) {
+    throw new SyntaxError("'name' is required.");
+  }
+
+  console.log(user.name); // undefined
+  console.log(user.age);  // 20
 } catch (error) {
-  console.log(error.name);
-  console.log(error.message);
+  console.log(`JSON Error: ${error.message}`);
 }
 
 /*
-Secara sintaksis, kode di atas tidak menimbulkan eror, sehingga blok catch akan diabaikan. Namun, tidak adanya properti name pada json sebenarnya sama saja dengan eror karena akan berdampak pada jalannya program kita.
-
-Untuk mengatasinya, kita bisa menggunakan throw
+Ketika properti user.name tidak memiliki nilai, maka program akan menghasilkan SyntaxError. Di dalamnya kita bisa menentukan pesan yang dapat membantu menjelaskan apa eror yang terjadi.
 */
