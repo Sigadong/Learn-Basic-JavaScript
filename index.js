7a3c1d02a66bd8c5128cb124eca700dd4f9d6dc9
Sigadong <irwanto.regar30@gmail.com>
2021-04-08 23:17:04 +0700

    Concurrency: onRejected with Catch Method.

--- index.js
+++ index.js
@@ -1,46 +1,24 @@
 // # CONCURRENCY
+
 /* Consuming Promises.
-Setelah mengetahui bagaimana membuat objek Promise, hal yang tentunya sangat penting adalah tahu bagaimana mengonsumsinya. Seperti yang telah kita bahas sebelumnya, status awal dari Promise adalah pending. Kemudian, akan ada dua kemungkinan yang terjadi, fulfilled atau rejected.
+Salah satu cara menulis kode yang baik adalah mengikuti prinsip yang disebut separation of concerns atau pemisahan masalah. Pemisahan masalah berarti mengorganisasikan kode ke dalam bagian-bagian yang berbeda berdasarkan tugas tertentu. Hal ini akan memudahkan kita kelak mencari kode yang salah jika aplikasi tidak bekerja dengan baik.
 
-Untuk menangani hasil dari Promise, kita gunakan method .then(). Jika kita terjemahkan, “then” berarti “kemudian”, sehingga kurang lebih kita memerintahkan JavaScript seperti ini: “Jika janji saya sudah selesai, kemudian lakukan ...”. Jika dituliskan dalam bentuk kode akan seperti berikut:
-
-    const myPromise = new Promise(executorFunction);
-    myPromise.then(onFullfilled, onRejected);
-
-.then() sendiri adalah sebuah higher-order function yang membutuhkan dua parameter. Keduanya adalah callback function yang juga dikenal sebagai handler. Handler pertama adalah fungsi yang akan dijalankan ketika Promise berstatus resolve. Sedangkan handler kedua adalah fungsi yang akan dijalankan ketika Promise berstatus reject.
+Perlu diketahui bahwa method .then() akan mengembalikan nilai promise yang sama dengan ketika objek promise itu dipanggil. Melalui sifatnya ini, daripada kita menetapkan logika resolve dan reject pada satu method then(), kita dapat memisahkan kedua logika tersebut menggunakan masing-masing method then() seperti ini:
 */
-
-const stock = {
-  coffeeBeans: 250,
-  water: 1000,
-}
-
-const checkStock = new Promise((resolve, reject) => {
-  if (stock.coffeeBeans >= 16 && stock.water >= 250) {
-    resolve("Stok cukup. Bisa membuat kopi");
-  } else {
-    reject("Stok tidak cukup");
-  }
-});
-
-
-const handleSuccess = resolvedValue => {
-  console.log(resolvedValue);
-}
-
-const handleFailure = rejectionReason => {
-  console.log(rejectionReason);
-}
-
-const check = checkStock().then(handleSuccess, handleFailure);
-
-console.log(check);
-
+checkStock()
+  .then(handleSuccess)
+  .then(null, handleFailure);
 
 /*
-Mari kita bedah kode di atas:
-checkStock() merupakan fungsi yang mengembalikan promise dan akan menghasilkan resolve() dengan membawa nilai “Stok cukup. Bisa membuat kopi”.
-Lalu kita mendeklarasikan fungsi handleSuccess() dan handleFailure() yang mencetak nilai dari parameternya.
-Kemudian kita memanggil method .then() dari checkStock. Isi parameter then() dengan dua fungsi handler yang telah kita buat sebelumnya.
-Parameter pertama berisi fungsi handleSuccess untuk menangani kondisi ketika promise berstatus resolve. Parameter kedua berisi fungsi handleFailure yang menangani ketika promise berstatus reject.
+Namun untuk menetapkan onRejected handler, kita perlu memberikan nilai null pada parameter pertama method .then(). Hal ini sedikit merepotkan bukan? Solusinya kita dapat menggunakan method lain, yakni .catch().
+
+Method .catch() mirip seperti .then(). Namun, method ini hanya menerima satu parameter function yang digunakan untuk rejected handler. Method catch() ini akan terpanggil ketika objek promise memiliki kondisi onRejected. Berikut contoh penggunaan method .catch(): 
+*/
+
+checkStock()
+  .then(handleSuccess)
+  .catch(handleFailure);
+
+/*
+Dengan menggunakan method catch(), kita dapat menerapkan prinsip separation of concerns sekaligus membuat kodenya menjadi lebih rapi.
  */
