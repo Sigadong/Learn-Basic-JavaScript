6990b1240d71a4134e3b2f31fccd652e6dd204c1
Sigadong <irwanto.regar30@gmail.com>
2021-04-08 12:06:49 +0700

    Concurrency: setTimeout()

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // # CONCURRENCY
-/* Dalam pemrograman, salah satu topik yang menantang adalah tentang concurrency. Concurrency sendiri berarti beberapa komputasi yang terjadi pada saat yang bersamaan[4]. Sejauh ini kita telah menuliskan kode secara synchronous. Lebih lanjut, pada modul ini kita akan mempelajari beberapa materi seperti:
-
-Bagaimana menjalankan program secara asynchronous
-Bagaimana menangani kode asynchronous
+/* setTimeout().
+Fungsi setTimeout() merupakan cara yang paling mudah untuk membuat kode kita dijalankan secara asynchronous. Fungsi ini menerima dua buah parameter. Parameter pertama adalah fungsi yang akan dijalankan secara asynchronous. Kedua adalah nilai number dalam milisecond sebagai nilai tunggu sebelum fungsi dijalankan. Contoh penggunaannya adalah seperti ini:
 */
 
-/* Synchronous vs Asynchronous.
-Dalam synchronous program, kode dijalankan secara berurutan dari atas ke bawah. Artinya jika kita menuliskan dua baris kode, maka baris kode kedua tidak bisa dieksekusi sebelum kode baris pertama selesai. Kita bisa bayangkan ini dalam kehidupan nyata ketika mengantri membeli kopi di sebuah kedai kopi. Kita tidak akan dilayani sebelum semua antrian di depan kita selesai dilayani, begitu pula orang di belakang kita pun harus menunggu gilirannya.
+console.log("Selamat datang!");
+setTimeout(() => {
+  console.log("Terima kasih sudah mampir, silakan datang kembali!");
+}, 3000);
 
-Dalam asynchronous program, jika kita menuliskan dua baris kode, kita dapat membuat baris kode kedua dieksekusi tanpa harus menunggu kode pada baris pertama selesai dieksekusi. Dalam dunia nyata kita bisa membayangkan dengan memesan kopi, tetapi pemesanannya melalui pelayan. Sembari menunggu pesannya datang, kita dapat melakukan aktivitas lain seperti membuka laptop, menulis, hingga kopi itu datang dengan sendirinya.
+console.log("Ada yang bisa dibantu?");
+
+/*
+Jika hanya mengenal program secara synchronous, maka kita dapat membayangkan hasilnya memiliki urutan sebagai berikut:
+
+Mencetak -> Selamat datang!
+Menunggu selama tiga detik.
+Mencetak -> Terima kasih sudah mampir, silakan datang kembali!
+Mencetak -> Ada yang bisa dibantu?
+Namun, nyatanya setTimeout() tidak akan menghentikan JavaScript untuk melakukan eksekusi kode pada baris berikutnya. Sehingga urutannya menjadi seperti berikut:
+
+Mencetak -> Selamat datang!
+Mencetak -> Ada yang bisa dibantu?
+Menunggu selama tiga detik
+Mencetak -> Terima kasih sudah mampir, silakan datang kembali!
  */
