8dacd63cc8cd61a86f91a9a0fa770e213084fd31
Sigadong <irwanto.regar30@gmail.com>
2021-04-08 12:05:04 +0700

    Concurrency: Synchronous vs Asynchronous.

--- index.js
+++ index.js
@@ -1,106 +1,12 @@
-// # ERROR HANDLING
+// # CONCURRENCY
+/* Dalam pemrograman, salah satu topik yang menantang adalah tentang concurrency. Concurrency sendiri berarti beberapa komputasi yang terjadi pada saat yang bersamaan[4]. Sejauh ini kita telah menuliskan kode secara synchronous. Lebih lanjut, pada modul ini kita akan mempelajari beberapa materi seperti:
 
-// Kuis Coding: Error Handling
-/** 
- * Saat ini, Anda sudah memiliki fungsi detectTriangle yang berguna untuk
- * mendeteksi jenis segitiga berdasarkan nilai argumen.
- * Contoh:
- *  - 1, 1, 1 -> Segitiga sama sisi
- *  - 4, 4, 2 -> Segitiga sama kaki
- *  - 3, 4, 6 -> Segitiga sembarang
- *
- * Namun fungsi detectTriangle belum berjalan dengan baik karena
- * bila ada argumen fungsi yang bukan number, alih-alih error, ia akan mengembalikan "Segitiga sembarang".
- * Contoh:
- *  - 1, false, 1 -> Segitiga sembarang
- *  - 'a', 3, 5 -> Segitiga sembarang
- *  - 12, 2, null -> Segitiga sembarang
- * Kondisi yang diharapkan:
- *  - 1, false, 1 -> Argumen kedua harus number
- *  - 'a', 3, 5 -> Argumen pertama harus number
- *  - 12, 2, null -> Argumen ketiga harus number
- *
- *  Tugas Anda adalah memperbaiki fungsi detectTriangle agar berjalan dengan kondisi yang diharapkan.
- *  Pastikan Anda menggunakan teknik Throwing dan Handling Error yah.
- *
- * TODO 1:
- * - Buatlah class ValidationError yang merupakan custom error dengan spesifikasi berikut:
- *   - Turunan dari class Error
- *   - Memiliki constructor(message)
- *   - this.name harus bernilai "ValidationError"
- *
- * TODO 2:
- * - Buatlah fungsi validateNumberInput yang memvalidasi 3 buah input (argumen) dengan spesifikasi berikut:
- *   - Menerima 3 argumen
- *   - Bila argumen pertama bukan number:
- *     - throw ValidationError dengan pesan 'Argumen pertama harus number'
- *   - Bila argumen kedua bukan number:
- *     - throw ValidationError dengan pesan 'Argumen kedua harus number'
- *   - Bila argumen ketiga bukan number:
- *     - throw ValidationError dengan pesan 'Argumen ketiga harus number'
- *
- * TODO 3:
- * - Panggil fungsi validateInputNumber di dalam fungsi detectTriangle untuk memvalidasi nilai argumen a, b, dan c.
- *   - pastikan Anda memanggil validaateInputNumber menggunakan try .. catch.
- *   - bila block catch terpanggil, kembalikan fungsi detectTriangle dengan pesan error yang dibawa fungsi validateInputNumber.
- */
-
-
-// TODO 1
-class ValidationError extends Error {
-  constructor(message) {
-    super(message);
-    this.name = "ValidationError";
-  }
-}
-
-// TODO 2
-const validateNumberInput = (a, b, c) => {
-  if (!Number.isInteger(a)) {
-    throw new ValidationError('Argumen pertama harus number');
-  } else if (!Number.isInteger(b)) {
-    throw new ValidationError('Argumen kedua harus number');
-  } else if (!Number.isInteger(c)) {
-    throw new ValidationError('Argumen ketiga harus number');
-  }
-}
-
-const detectTriangle = (a, b, c) => {
-  // TODO 3
-  try {
-    validateNumberInput(a, b, c);
-  } catch (error) {
-    if (error instanceof SyntaxError) {
-      return error.message;
-    } else if (error instanceof ValidationError) {
-      return error.message;
-    } else if (error instanceof ReferenceError) {
-      return error.message;
-    } else {
-      return error.stack;
-    }
-  }
-
-  if (a === b && b === c) {
-    return 'Segitiga sama sisi';
-  }
-
-  if (a === b || a === c || b === c) {
-    return 'Segitiga sama kaki';
-  }
-
-  return 'Segitiga sembarang';
-};
-
-const detectTriangles = detectTriangle('a', 3, 2);
-console.log(detectTriangles);
-
-
-/*
-Pada modul ini kita mempelajari bagaimana menangani kemungkinan eror pada program agar tidak mengalami crash.
-
-Beberapa poin yang sudah kita bahas di antaranya:
- - Penggunaan sintaksis try-catch untuk menangani eror.
- - Melemparkan eror sendiri yang tidak terdeteksi oleh JavaScript.
- - Membuat kelas sendiri untuk menandai eror yang tidak tersedia di JavaScript.
+Bagaimana menjalankan program secara asynchronous
+Bagaimana menangani kode asynchronous
 */
+
+/* Synchronous vs Asynchronous.
+Dalam synchronous program, kode dijalankan secara berurutan dari atas ke bawah. Artinya jika kita menuliskan dua baris kode, maka baris kode kedua tidak bisa dieksekusi sebelum kode baris pertama selesai. Kita bisa bayangkan ini dalam kehidupan nyata ketika mengantri membeli kopi di sebuah kedai kopi. Kita tidak akan dilayani sebelum semua antrian di depan kita selesai dilayani, begitu pula orang di belakang kita pun harus menunggu gilirannya.
+
+Dalam asynchronous program, jika kita menuliskan dua baris kode, kita dapat membuat baris kode kedua dieksekusi tanpa harus menunggu kode pada baris pertama selesai dieksekusi. Dalam dunia nyata kita bisa membayangkan dengan memesan kopi, tetapi pemesanannya melalui pelayan. Sembari menunggu pesannya datang, kita dapat melakukan aktivitas lain seperti membuka laptop, menulis, hingga kopi itu datang dengan sendirinya.
+ */
