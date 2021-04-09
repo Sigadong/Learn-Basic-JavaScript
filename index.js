8cc4dc322a036ed91c605ed4eca4eec30a0e60e6
Sigadong <irwanto.regar30@gmail.com>
2021-04-09 10:31:53 +0700

    Concurrency: Async-await - Part 2.

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 // # CONCURRENCY
 
 /* Async-await.
-Pembahasan terakhir mengenai asynchronous process kali ini adalah penggunaan syntax async-await. Apa itu?
+Namun, sejak ES8 (ECMAScript 2017) kita dapat menuliskan asynchronous process layaknya synchronous process dengan bantuan keyword async dan await.
 
-Seperti yang kita tahu, penulisan kode asynchronous sedikit berbeda dengan proses synchronous. Contohnya, untuk mendapatkan nilai coffee dari sebuah proses asynchronous, kita tidak dapat melakukannya dengan teknik seperti ini:
+Fitur async/await sebenarnya hanya syntactic sugar. Itu berarti secara fungsionalitas bukanlah sebuah fitur baru dalam JavaScript. Namun, hanya gaya penulisan baru yang dikembangkan dari kombinasi penggunaan Promise dan generator (pembahasan mengenai generator bisa Anda pelajari di sini). Sehingga, async/await ini tidak dapat digunakan jika tidak ada Promise.
 
-  function makeCoffee() {
-    const coffee = getCoffee(); // async process menggunakan promise
-    console.log(coffee);
-}
-makeCoffee();
-
-Melainkan harus seperti ini:
+Lantas bagaimana cara menggunakan async/await ini? Pada contoh kode sebelumnya, mari kita lihat juga fungsi getCoffee() dan bagaimana ia mengembalikan promise.
 */
-// Promise.
-function makeCoffee() {
-  getCoffee().then(coffee => {
-    console.log(coffee);
-  });
+
+const getCoffee = () => {
+  return new Promise((resolve, reject) => {
+    const seeds = 100;
+    setTimeout(() => {
+      if (seeds >= 10) {
+        resolve("Kopi didapatkan!");
+      } else {
+        reject("Biji kopi habis!");
+      }
+    }, 1000);
+  })
 }
-// makeCoffee();
 
+// Asynchronous Menggunakan .then().
+// function makeCoffee() {
+//   getCoffee().then(coffee => {
+//     console.log(coffee);
+//   });
+// }
 
-// Callback.
-function makeCoffee() {
-  getCoffee(function (coffee) {
-    console.log(coffee);
-  });
+async function makeCoffee() {
+  const coffee = await getCoffee();
+  console.log(coffee);
 }
 
 makeCoffee();
-
 
 
 /**
+ Keyword async digunakan untuk memberitahu JavaScript supaya menjalankan fungsi makeCoffee() secara asynchronous. Lalu, keyword await digunakan untuk menghentikan proses pembacaan kode selanjutnya sampai fungsi getCoffee() mengembalikan nilai promise resolve.
+
+Walaupun await menghentikan proses pembacaan kode selanjutnya pada fungsi makeCoffee, tetapi ini tidak akan mengganggu proses runtime sesungguhnya pada JavaScript (global). Karena fungsi makeCoffee berjalan secara asynchronous, kita tidak dapat menggunakan await tanpa membuat function dalam scope-nya berjalan secara asynchronous.
  */
