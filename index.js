164f9a06e7d7808218c7f6513bd2ab4c9d123984
Sigadong <irwanto.regar30@gmail.com>
2021-04-09 11:25:29 +0700

    Cuncurrency: Chaining Promise using async-await.

--- index.js
+++ index.js
@@ -1,33 +1,88 @@
 // # CONCURRENCY
 
-/* Handle onRejected using async-await.
-Perlu jadi catatan bahwa await hanya akan mengembalikan nilai jika promise berhasil dilakukan (onFulfilled). Lantas bagaimana jika promise gagal dilakukan (onRejected)? Caranya cukup sederhana. Kembali lagi kepada prinsip synchronous code. Kita dapat menangani sebuah eror atau tolakan dengan menggunakan try...catch.
+/* Chaining Promise using async-await
+Pertanyaan selanjutnya adalah bagaimana melakukan promise berantai bila menggunakan async/await? Jawabannya adalah sama seperti ketika kita mendapatkan nilai dari function yang berjalan secara synchronous.
 
-Ketika menggunakan async/await, biasakan ketika mendapatkan resolved value dari sebuah promise, untuk menempatkannya di dalam blok try seperti ini:
+Dengan pendekatan async-await, maka kode mesin kopi kita akan menjadi seperti ini:
 */
 
-const getCoffee = () => {
+const state = {
+  stock: {
+    coffeeBeans: 250,
+    water: 1000,
+  },
+  isCoffeeMachineBusy: false,
+}
+const checkAvailability = () => {
   return new Promise((resolve, reject) => {
-    const seeds = 100;
     setTimeout(() => {
-      if (seeds >= 10) {
-        resolve("Kopi didapatkan!");
+      if (!state.isCoffeeMachineBusy) {
+        resolve("Mesin kopi siap digunakan.");
       } else {
-        reject("Biji kopi habis!");
+        reject("Maaf, mesin sedang sibuk.");
       }
+    }, 1000);
+  });
+};
+const checkStock = () => {
+  return new Promise((resolve, reject) => {
+    state.isCoffeeMachineBusy = true;
+    setTimeout(() => {
+      if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
+        resolve("Stok cukup. Bisa membuat kopi.");
+      } else {
+        reject("Stok tidak cukup!");
+      }
+    }, 1500);
+  });
+};
+
+// Dua Fungsi Tambahan (boilWater & grindCoffeeBeans).
+const boilWater = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Memanaskan air...");
+    setTimeout(() => {
+      resolve("Air panas sudah siap!");
+    }, 2000);
+  })
+}
+
+const grindCoffeeBeans = () => {
+  return new Promise((resolve, reject) => {
+    console.log("Menggiling biji kopi...");
+    setTimeout(() => {
+      resolve("Kopi sudah siap!");
     }, 1000);
   })
 }
 
-async function makeCoffee() {
+const brewCoffee = () => {
+  console.log("Membuatkan kopi Anda...")
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Kopi sudah siap!")
+    }, 2000);
+  });
+};
+
+async function makeEspresso() {
   try {
-    const coffee = await getCoffee();
+    await checkAvailability();
+    await checkStock();
+
+    await Promise.all([boilWater(), grindCoffeeBeans()]);
+
+    const coffee = await brewCoffee();
     console.log(coffee);
   } catch (rejectedReason) {
     console.log(rejectedReason);
   }
 }
 
-makeCoffee();
+makeEspresso();
 
+/*
+NB:
+Async/await ini menjadi fitur baru yang sangat berguna. Terlebih untuk kita yang lebih nyaman menangani proses asynchronous dengan menggunakan gaya synchronous.
+*/
 
