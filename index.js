3845b1d986cd2b9e0f0a68d0fc8e71cca4f27584
Sigadong <irwanto.regar30@gmail.com>
2021-04-09 10:22:55 +0700

    Concurrency: Async-await - Part 1

--- index.js
+++ index.js
@@ -1,109 +1,37 @@
 // # CONCURRENCY
 
-/* Promise All.
-Pada materi sebelumnya kita belajar bagaimana promise dapat menangani situasi di mana terdapat asynchronous process yang saling membutuhkan untuk melaksanakan tugasnya. Lalu bagaimana jika kita ingin menjalankan banyak promise sekaligus tanpa memedulikan urutan? Bukankah concurrency memungkinkan kita melakukan banyak proses bersamaan agar lebih efisien?
+/* Async-await.
+Pembahasan terakhir mengenai asynchronous process kali ini adalah penggunaan syntax async-await. Apa itu?
 
-Ketika pergi ke sebuah kedai kopi bersama rekan kerja, kita biasanya memesan kopi secara bersamaan. Meskipun kopi yang kita pesan berbeda, tak jarang pelayanan mengantarkan pesanan bersamaan. Nah, pada kasus inilah pelayan menggunakan teknik Promise.all().
+Seperti yang kita tahu, penulisan kode asynchronous sedikit berbeda dengan proses synchronous. Contohnya, untuk mendapatkan nilai coffee dari sebuah proses asynchronous, kita tidak dapat melakukannya dengan teknik seperti ini:
 
-Method Promise.all() dapat menerima banyak promise dalam bentuk array pada parameternya. Kemudian method tersebut akan mengembalikan nilai seluruh hasil dari promise dalam bentuk array.
+  function makeCoffee() {
+    const coffee = getCoffee(); // async process menggunakan promise
+    console.log(coffee);
+}
+makeCoffee();
+
+Melainkan harus seperti ini:
 */
-
-/*
-Keduanya dapat berjalan bersamaan. Kita akan memanfaatkan Promise.all() untuk menjalankan kedua fungsi di atas sebelum fungsi brewCoffee(). Ubah kode fungsi makeEspresso() menjadi seperti ini:
-*/
-
-const state = {
-  stock: {
-    coffeeBeans: 250,
-    water: 1000,
-  },
-  isCoffeeMachineBusy: false,
-}
-
-const checkAvailability = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (!state.isCoffeeMachineBusy) {
-        resolve("Mesin kopi siap digunakan.");
-      } else {
-        reject("Maaf, mesin sedang sibuk.");
-      }
-    }, 1000);
+// Promise.
+function makeCoffee() {
+  getCoffee().then(coffee => {
+    console.log(coffee);
   });
-};
+}
+// makeCoffee();
 
-const checkStock = () => {
-  return new Promise((resolve, reject) => {
-    state.isCoffeeMachineBusy = true;
-    setTimeout(() => {
-      if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
-        resolve("Stok cukup. Bisa membuat kopi.");
-      } else {
-        reject("Stok tidak cukup!");
-      }
-    }, 1500);
+
+// Callback.
+function makeCoffee() {
+  getCoffee(function (coffee) {
+    console.log(coffee);
   });
-};
-
-// Dua Fungsi Tambahan (boilWater & grindCoffeeBeans).
-const boilWater = () => {
-  return new Promise((resolve, reject) => {
-    console.log("Memanaskan air...");
-    setTimeout(() => {
-      resolve("Air panas sudah siap!");
-    }, 2000);
-  })
 }
 
-const grindCoffeeBeans = () => {
-  return new Promise((resolve, reject) => {
-    console.log("Menggiling biji kopi...");
-    setTimeout(() => {
-      resolve("Kopi sudah siap!");
-    }, 1000);
-  })
-}
+makeCoffee();
 
-const brewCoffee = () => {
-  console.log("Membuatkan kopi Anda...")
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Kopi sudah siap!")
-    }, 2000);
-  });
-};
-
-function makeEspresso() {
-  checkAvailability()
-    .then((value) => {
-      console.log(value);
-      return checkStock();
-    })
-    .then(value => {
-      console.log(value);
-      const promises = [boilWater(), grindCoffeeBeans()];
-      return Promise.all(promises);
-    })
-    .then((value) => {
-      console.log(value)
-      return brewCoffee();
-    })
-    .then(value => {
-      console.log(value);
-      state.isCoffeeMachineBusy = false;
-    })
-    .catch(rejectedReason => {
-      console.log(rejectedReason);
-      state.isCoffeeMachineBusy = false;
-    });
-}
-
-makeEspresso();
 
 
 /**
- *
- Ketika kode di atas dieksekusi, kita perlu menunggu dua (2) detik untuk proses boilWater dan grindCoffeeBeans (durasi terlama dari promise yang dijalankan dari Promise.all()). Ini menunjukkan bahwa semua promise di dalam Promise.all() berjalan bersamaan dan menunggu sampai semua proses di dalamnya selesai dijalankan.
-
-Yang perlu kita perhatikan, urutan nilai yang dihasilkan oleh method ini sesuai dengan promise yang kita tentukan pada parameternya.
  */
