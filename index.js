d7eceeaffd0da814bb2cac71fd070a73d17bc5ae
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 11:40:20 +0700

    Data Structure: Destructuring Object.

--- index.js
+++ index.js
@@ -1,8 +1,109 @@
 // # JAVASCRIPT FUNDAMENTALS
 
 /* Data Structure.
-  - Destructuring Object & Array
-Literasi object dan array adalah dua hal yang paling banyak digunakan dalam mengelola data di JavaScript. JSON (JavaScript Object Notation) merupakan format data paling populer yang digunakan dalam transaksi data saat ini.
+  - Destructuring Object
+Penulisan sintaksis destructuring object pada ES6 menggunakan object literal ({ }) di sisi kiri dari operator assignment.
+*/
+const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
 
-Apa sebenarnya destructuring object dan array itu? Destructuring dalam JavaScript merupakan sintaksis yang dapat mengeluarkan nilai dari array atau properties dari sebuah object ke dalam satuan yang lebih kecil.
+const { firstName, lastName, age } = profile;
+console.log(firstName, lastName, age);
+
+/* output:
+John Doe 18
+*/
+
+
+/* Destructuring Assignment
+Pada contoh sebelumnya, kita telah melakukan destructuring object pada deklarasi variabel. Namun, pada kasus tertentu mungkin kita perlu melakukannya pada variabel yang sudah dideklarasikan.
+*/
+const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
+
+let firstName = "Dimas";
+let age = 20;
+
+// menginisialisasi nilai baru melalui object destruction
+({ firstName, age } = profile);
+
+console.log(firstName);
+console.log(age);
+
+/* output:
+John
+18
+*/
+
+
+/*
+Default Values.
+Ketika kita mendestruksikan objek dan menetapkan variabel dengan nama yang bukan merupakan properti dari objek, maka nilai dari variabel tersebut menjadi undefined. Contohnya:
+*/
+const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
+
+const { firstName, age, isMale } = profile;
+
+console.log(firstName)
+console.log(age)
+console.log(isMale)
+
+/* output:
+John
+18
+undefined
+*/
+
+
+/*
+Alternatifnya, kita bisa secara opsional mendefinisikan nilai default pada properti tertentu jika tidak ditemukan. Untuk melakukanya, tambahkan tanda assignment (=) setelah nama variabel dan tentukan nilai default-nya seperti ini:
+*/
+const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
+const { firstName, age, isMale = false } = profile;
+
+console.log(firstName)
+console.log(age)
+console.log(isMale)
+
+/* output:
+John
+18
+false
+*/
+
+
+/* Assigning to Different Local Variable Names
+Sampai saat ini kita tahu bahwa untuk melakukan destrukturisasi object pada variabel lokal, kita perlu menyeragamkan penamaan variabel lokal dengan properti object-nya. Namun, sebenarnya dalam proses destrukturisasi object kita bisa menggunakan penamaan variabel lokal yang berbeda. ES6 menyediakan sintaksis tambahan yang membuat kita dapat melakukan hal tersebut. Penulisannya mirip seperti ketika kita membuat properti beserta nilainya pada object.
+*/
+
+const profile = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 18
+}
+
+const { firstName: localFirstName, lastName: localLastName, age: localAge } = profile;
+
+console.log(localFirstName);
+console.log(localLastName);
+console.log(localAge);
+
+/* output:
+John
+Doe
+18
 */
