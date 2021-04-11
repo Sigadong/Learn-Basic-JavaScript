8202a52b13f4ab4fc623df3ef79d8ea7bd99e87c
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 09:36:38 +0700

    JavaScript Fundamentals: Data Type-> Numbers.

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - Undefined
+/* - Numbers
 
-Tipe data ini terbentuk ketika sebuah variabel tidak memiliki nilai. Artinya, ketika kita mendeklarasikan variabel tanpa menginisialisasikan nilainya, variabel tersebut menjadi undefined.
+Jika angka tersebut merupakan sebuah bilangan desimal, maka kita bisa gunakan tanda titik pada pecahan bilangannya.
 */
 
-let x;
-console.log(typeof (x));
+let x = 10;
+console.log(typeof (x))
 
-/* output: undefined */
+/* output: number */
 
+let y = 17.25;
+console.log(typeof (y))
+
+/* output: number */
 
 
 /**
+ Pada operator aritmatika juga terdapat operator increment (++) dan decrement (--). Operator increment dan decrement digunakan untuk menambahkan atau mengurangi nilai 1 pada nilai variabel yang ada sekarang.
+
+Operator ini dapat dituliskan sebelum atau sesudah variabel, tetapi hal tersebut bukan berarti sama. Berikut ketentuannya:
+* Jika dituliskan setelah variabel (x++), expression akan menghasilkan nilai variabel sebelum ditingkatkan nilainya.
+* Jika dituliskan sebelum variabel (++x), expression akan menghasilkan nilai variabel setelah ditingkatkan nilainya.
+
+Untuk lebih jelasnya, berikut adalah contoh kode penerapan operator tersebut, perhatikan hasil yang didapat.
  */
+
+/* Increment dan Decrement */
+
+let postfix = 5;
+console.log(postfix++);
+/* output: 5 */
+console.log(postfix);
+/* output: 6 */
+
+let prefix = 5;
+console.log(++prefix);
+/* output: 6 */
