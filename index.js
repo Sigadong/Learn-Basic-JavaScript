e5c8491ac3a15c3720dcb92a8601aae2d70ca496
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 09:25:20 +0700

    JavaScript Fundamentals: Variable

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 // # JAVASCRIPT FUNDAMENTALS
-/*
-  - Variable
-  - Data Type
-  - Operator
-  - If / Else Statement
-  - Switch Case Statement
-  - Loop
+
+/* - Variable.
+Pada JavaScript setidaknya ada tiga cara untuk mendeklarasikan sebuah variabel, yaitu menggunakan keyword var, let, dan const. Pada versi ECMAScript 2015 (ES6) dikenalkan deklarasi variabel dengan let dan const untuk menggantikan var yang dinilai kontroversial dan rawan menimbulkan bug
 */
+
+let lastName = "Skywalker";
+console.log(lastName);
+
+/**
+ Setiap variabel memiliki nama yang dapat kita panggil dan gunakan. Kita dapat menamai variabel dengan nama apa pun, tetapi pastikan penamaannya masih masuk akal dengan konteksnya supaya kode mudah di-maintenance.
+
+Sebaiknya hindari penamaan variabel dengan istilah umum seperti “data”. Gunakanlah penamaan variabel yang dapat mendeskripsikan nilai dari variabel itu sendiri. Berikut beberapa aturan dalam penamaan variabel yang perlu Anda ketahui:
+
+- Harus dimulai dengan huruf atau underscore (_).
+- Dapat terdiri dari huruf, angka, dan underscore (_) dengan berbagai kombinasi.
+- Tidak boleh mengandung spasi (whitespace). Jika penamaan variabel lebih dari dua kata, tuliskan secara camelCase. Contoh firstName, lastName, catName, dll.
+- Tidak boleh mengandung karakter spesial (! . , / \ + * = dll.)
+ */
