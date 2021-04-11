cee42c99c4682b37ad587723444b5b96815f1883
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 09:29:49 +0700

    JavaScript Fundamentals: Data Type

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - Variable.
-Pada JavaScript setidaknya ada tiga cara untuk mendeklarasikan sebuah variabel, yaitu menggunakan keyword var, let, dan const. Pada versi ECMAScript 2015 (ES6) dikenalkan deklarasi variabel dengan let dan const untuk menggantikan var yang dinilai kontroversial dan rawan menimbulkan bug
+/* - Data Type.
+Nilai yang kita tetapkan pada variabel pasti memiliki tipe data. Tipe data merupakan pengklasifikasian data berdasarkan jenisnya. Pada JavaScript terdapat beberapa tipe data sebagai berikut:
+- Undefined
+- Numbers
+- BigInt
+- Strings
+- Boolean
+- Null
+- Symbol
+
+Seluruh Data type diatas termasuk kedalam tipe data PRIMITIF.
 */
 
-let lastName = "Skywalker";
-console.log(lastName);
 
 /**
- Setiap variabel memiliki nama yang dapat kita panggil dan gunakan. Kita dapat menamai variabel dengan nama apa pun, tetapi pastikan penamaannya masih masuk akal dengan konteksnya supaya kode mudah di-maintenance.
 
-Sebaiknya hindari penamaan variabel dengan istilah umum seperti “data”. Gunakanlah penamaan variabel yang dapat mendeskripsikan nilai dari variabel itu sendiri. Berikut beberapa aturan dalam penamaan variabel yang perlu Anda ketahui:
-
-- Harus dimulai dengan huruf atau underscore (_).
-- Dapat terdiri dari huruf, angka, dan underscore (_) dengan berbagai kombinasi.
-- Tidak boleh mengandung spasi (whitespace). Jika penamaan variabel lebih dari dua kata, tuliskan secara camelCase. Contoh firstName, lastName, catName, dll.
-- Tidak boleh mengandung karakter spesial (! . , / \ + * = dll.)
  */
