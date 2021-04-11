9301edd878bf8509b6e05adbd8542cf1b39f3c63
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 09:54:06 +0700

    JavaScript Fundamentals: Data Type-> Symbol.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - Null.
-Tipe berikutnya adalah null. Serupa dengan undefined, namun null perlu diinisialisasikan pada variabel. Null biasa digunakan sebagai nilai sementara pada variabel, tapi sebenarnya nilai tersebut “tidak ada”.
+/* - Symbol.
+Symbol adalah tipe data baru yang dikenalkan pada ES6. Tipe data Symbol digunakan untuk menunjukkan identifier yang unik. Ketika membuat Symbol, kita bisa memberikan deskripsi atau nama symbol seperti ini:
+*/
+const id = Symbol("id");
+console.log(id);
 
-Terkadang kita perlu membuat sebuah variabel, namun kita belum memerlukan nilai apa-apa dan tidak ingin terikat oleh tipe data apa pun. Nah, daripada kita tidak menetapkan nilai apa pun (variabel akan undefined) sebaiknya kita beri nilai null pada variabel tersebut dan ubah nanti ketika kita membutuhkannya.
-
-Untuk menetapkan null pada variabel, kita dapat gunakan keyword null ketika variabel tersebut diinisialisasi.
+/* output
+Symbol(id)
 */
 
-let someLaterData = null;
-console.log(someLaterData);
 
-/* output:
-null
+/* 
+Symbol disebut sebagai identifier yang unik karena meskipun kita membuat dua variabel symbol dengan nama atau deskripsi yang sama, kedua nilainya tetap dianggap berbeda. Contohnya lihat kode berikut:
+*/
+const id1 = Symbol("id");
+const id2 = Symbol("id");
+console.log(id1 == id2);
+
+/* output
+false
+*/
+
+
+/*
+Symbol ini umumnya digunakan sebagai nama property dari Object. Object sendiri merupakan tipe data kompleks untuk menyimpan berbagai struktur data.
 */
