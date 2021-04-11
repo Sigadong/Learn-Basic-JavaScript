00c02e55f5de3470bfb626f4cdf69503efb61761
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 18:23:02 +0700

    Object-Oriented Programming (OOP): 4 Pilar OOP -> Encapsulation.

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
 // # Object-Oriented Programming (OOP)
 
-/* #Property & Method Property.
-  - Constructor
-Ketika kita membuat sebuah objek, adakalanya karakteristik dari blueprint yang kita buat harus sudah didefinisikan bersamaan dengan sebuah objek saat pertama kali diinisiasi. Constructor adalah sebuah method/function yang dijalankan pertama kali ketika object dibuat. Dari contoh kelas yang kita buat sebelumnya, kita akan membuat from sebagai sebuah value yang harus ditulis ketika sebuah objek di inisiasi. Maka dalam JavaScript ada dua cara, yaitu:
+/* # 4 Pilar OOP
+Seperti yang sempat kita singgung pada awal modul OOP, terdapat empat pilar dalam OOP, yaitu encapsulation, abstraction, inheritance, dan polymorphism.
+
+  - Encapsulation
+Enkapsulasi adalah kondisi di mana attribute atau method di dalam class dibungkus dan bersifat privat. Artinya objek lain tidak bisa mengakses atau mengubah nilai dari property secara langsung. Pada contoh kasus Mail kita tidak bisa langsung mengubah daftar contact, namun kita bisa menambahkannya melalui fungsi saat kirim pesan atau mengambil data tersebut melalui method showAllContacts.
  */
-// cara 1, jika kita menggunakan statement class
-class YourClassName {
-  constructor(params1, params2, ....) {
-    // do something here
-  };
-}
 
 
-// cara 2, jika kita menggunakan statemen function
-function Mail(params1, params2, ....) {
-  this.yourPropertyName = params1;
-  // do something here
-}
-
-
-/* Contoh penerapannya sebagai berikut: */
-
-// cara 1
 class Mail {
+  _contacts = [];
   constructor(author) {
-    this.from = 'pengirim@dicoding.com';
-
-    console.log('is instantiated', author);
-  };
+    this.from = author;
+  }
+  sendMessage = function (msg, to) {
+    console.log('you send:', msg, 'to', to, 'from', this.from);
+    this._contacts.push(to);
+  }
+  showAllContacts() {
+    return this._contacts;
+  }
 }
-
-// cara 2
-function Mail(author) {
-  this.from = author;
-
-  console.log('is instantiated', author);
-}
-
-
-//Dari contoh constructor di atas, maka cara pemanggilannya menjadi seperti di bawah ini:
-const mail1 = new Mail("emailku@dicoding.com");
-
-//Karena JavaScript bukan bahasa dengan dukungan static type maka sebenarnya kita dapat melakukan instansiasi dengan parameter sesuka kita:
-const mail1 = new Mail(085000111222); // misalkan untuk SMS
-const mail2 = new Mail("emailku@dicoding.com"); // misalkan untuk Email
