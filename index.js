0184c7d6b7f6cbc6f4f7f3ab90f2a7e82caa573f
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 10:17:34 +0700

    JavaScript Fundamentals: If / Else Statement -> Truthy & Falsy.

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - If/Else Statement
-Setiap hari kita melakukan perhitungan dan perbandingan guna membuat keputusan, apa pun itu. Contohnya, apakah perlu mencuci kendaraan ketika cuaca sedang cerah? Apa saja transportasi online yang bisa dipesan ketika hujan untuk sampai di tempat tujuan?
+/* - Truthy & Falsy.
+Di dalam if statement kita perlu memasukkan expression yang akan dievaluasi. Umumnya, expression tersebut mengembalikan nilai boolean untuk menentukan kondisi true atau false. Lalu bagaimana jika kita menuliskan expression yang tidak mengembalikan nilai boolean? Jawabannya bisa.
+
+Setiap nilai pada JavaScript pada dasarnya juga mewarisi sifat boolean. Nilai ini dikenal dengan truthy atau falsy. Nilai truthy berarti nilai yang ketika dievaluasi akan menghasilkan nilai true, begitu pula falsy bernilai false. Jadi manakah yang termasuk truthy dan falsy? Selain nilai boolean false, tipe data atau nilai yang dianggap falsy, antara lain:
+  - Number -> 0
+  - BigInt -> 0n
+  - String kosong seperti “” atau ‘’
+  - null
+  - undefined
+  - NaN, atau Not a Number
+
+Selain contoh di atas maka nilainya adalah truthy dan ketika dievaluasi ke dalam if statement akan bernilai true. Berikut ini contohnya dalam kode:
 */
 
-let language = "French";
-let greeting = "Selamat Pagi"
-
-if (language === "English") {
-  greeting = "Good Morning!";
-} else if (language === "French") {
-  greeting = "Bonjour!"
-} else if (language === "Japanese") {
-  greeting = "Ohayou Gozaimasu!"
+let name = "";
+if (name) {
+  console.log(`Halo, ${name}`);
+} else {
+  console.log("Nama masih kosong");
 }
-console.log(greeting);
-/* output
-Bonjour!
-*/
 
-
-
-// condition ? true expression : false expression
-const isMember = false;
-const discount = isMember ? 0.1 : 0;
-console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
-
-/* output
-Anda mendapatkan diskon sebesar 0%
- */
-
-/*
-Ternary operator membutuhkan tiga operand. Sebelum tanda tanya (?) berisi kondisi yang ingin kita evaluasi. Kemudian diikuti dengan expression apabila nilai kondisinya benar sebelum tanda titik dua. Terakhir adalah expression yang dieksekusi ketika kondisinya salah. Karena merupakan conditional expression, maka operand kedua dan ketiga harus mengembalikan nilai
+/* output:
+Nama masih kosong
  */
