62b65462a5b6530f02a3773b222676c32fad0bd7
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 09:49:37 +0700

    JavaScript Fundamentals: Data Type-> Boolean.

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - Strings
+/* - Boolean.
+Boolean hanya memiliki dua nilai, yaitu true atau false. Tipe data ini menjadi kunci utama dalam penentuan logika. Kita akan banyak menggunakannya nanti dalam materi if/else statement. Untuk menetapkan nilai boolean pada variabel, gunakan keyword true atau false seperti di bawah ini.
 
-Tipe data selanjutnya adalah string yang merupakan sebuah teks. Untuk menetapkan nilai sebagai string pada variabel gunakan tanda petik satu (‘) atau petik dua (“) di antara teksnya. Contohnya:
+Boolean hanya memiliki dua nilai, yaitu true atau false. Tipe data ini menjadi kunci utama dalam penentuan logika. Kita akan banyak menggunakannya nanti dalam materi if/else statement. Untuk menetapkan nilai boolean pada variabel, gunakan keyword true atau false seperti di bawah ini.
 */
+let x = true;
+let y = false;
 
-let greet = "Hello";
-console.log(typeof (greet))
+console.log(typeof (x))
+console.log(typeof (y))
 
-/* output: string */
-
-
-/**
- Lalu bagaimana jika teks memiliki kedua tanda petik seperti ini?
- */
-const answer = '"I think it's awesome!" he answered confidently';
-console.log(answer);
-
-/*
-Tentunya kode di atas akan menghasilkan eror. Solusinya, gunakan backslash(\) untuk mengurangi ambiguitas dalam tanda petik. Mekanisme ini juga dikenal dengan nama escape string. Sehingga kode di atas akan menjadi seperti berikut:
-*/
-
-const answer = '"I think it\'s awesome!" he answered confidently';
-
-/*
-Backslash sebelum tanda petik akan memberitahukan interpreter bahwa itu hanyalah tanda petik dan tidak boleh ditafsirkan sebagai pembatas string. Selain tanda petik, backslash juga berguna untuk mengabaikan simbol lain yang menimbulkan ambigu di dalam string, contohnya seperti backslash itu sendiri.
-*/
-
-/*
-Selain concatenation, string pada JavaScript juga mendukung string interpolation. Sederhananya, kita bisa memasukkan variabel ke dalam sebuah string template. Contohnya adalah seperti berikut:
-*/
-
-const myName = "Luke";
-console.log(`Hello, my name is ${myName}.`);
-
-/* output: Hello, my name is Luke. */
-
-/*
-Perhatikan bahwa untuk mendefinisikan string template, Anda perlu menggunakan backticks (`), biasanya terletak di keyboard di sebelah kiri tombol 1). Di dalam string letakkan variabel yang ingin dimasukkan ke dalam placeholder ${myName}.
+/* output:
+boolean
+boolean
 */
