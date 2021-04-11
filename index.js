49f3105e439549d4b07ca1553d98ec05adeb6d05
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 12:11:48 +0700

    Data Structure: Kuis Coding: Array.

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* Kuis Coding: Object*/
+/* Kuis Coding: Array */
 
 /**
- * TODO
- * 1. Buatlah variabel dengan nama restaurant yang object dengan ketentuan berikut:
- *    - Memiliki properti bernama "name"
- *       - Bertipe data string
- *       - Bernilai apa pun, asalkan tidak string kosong atau null.
- *    - Memiliki properti bernama "city"
- *       - Bertipe data string
- *       - Bernilai apa pun, asalkan tidak string kosong atau null.
- *    - Memiliki properti "favorite drink"
- *       - Bertipe data string
- *       - Bernilai apa pun, asalkan tidak string kosong atau null.
- *    - Memiliki properti "favorite food"
- *       - Bertipe data string
- *       - Bernilai apa pun, asalkan tidak string kosong atau null.
- *    - Memiliki properti "isVegan"
- *       - Bertipe data boolean
- *       - Bernilai boolean apa pun.
+ * TODO:
+ * Buatlah sebuah variabel dengan nama evenNumber yang merupakan sebuah array dengan ketentuan:
+ *   - Array tersebut menampung bilangan genap dari 1 hingga 100
  *
- * 2. Buatlah variabel bernama name.
- *    Kemudian isi dengan nilai name dari properti object restaurant
- * 3. Buatlah variabel bernama favoriteDrink.
- *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ * Catatan:
+ *   - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada array.
  */
 
-
 // TODO
-const restaurant = {
-  name: "iresto",
-  city: "Medan",
-  "favorite drink": "Ice Alvokat",
-  "favorite food": "Sushi",
-  isVegan: true,
+let evenNumber = [];
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    evenNumber.push(i);
+  }
 }
 
-const name = restaurant.name;
-const favoriteDrink = restaurant["favorite drink"];
+console.log(evenNumber);
 
-console.log(name);
-console.log(favoriteDrink);
