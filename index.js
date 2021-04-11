ba103b6671098ca4864e3d1cbb9452354973e7bb
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 10:31:40 +0700

    JavaScript Fundamentals: Loop -> For Of Loop.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 // # JAVASCRIPT FUNDAMENTALS
 
 /* - Loop.
-For loop.
-Dari beberapa cara melakukan proses loop pada JavaScript, “for” merupakan salah satu cara yang banyak digunakan. Struktur dasar dari for tampak seperti berikut:
+For of loop
+Cara lain dalam melakukan looping adalah menggunakan for..of. For of mulai hadir pada ECMAScript 2015 (ES6). Cara ini jauh lebih sederhana dan modern dibanding for loop biasa. Sintaks dasar dari for of loop adalah seperti ini:
 
-for(inisialisasi variabel; test kondisi; perubahan nilai variabel) {
+for(arrayItem of myArray) {
     // do something
 }
-Berikut ini contoh penerapannya secara nyata:
+
+Dengan for..of nilai tiap array akan diinisialisasi pada variabel baru yang kita tentukan pada tiap proses looping-nya. Jumlah proses looping-nya pun akan menyesuaikan dengan ukuran dari array. Sederhananya seperti kita melakukan perintah “Hei JavaScript! Lakukan perulangan pada myArray, akses tiap nilainya, dan simpan pada variabel arrayItem”. Pada proses looping kita gunakan variabel arrayItem untuk mengakses tiap nilai dari item myArray
 */
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
+let myArray = ["Luke", "Han", "Chewbacca", "Leia"];
+
+for (const arrayItem of myArray) {
+  console.log(arrayItem)
 }
 
 /* output
-0 1 2 3 4 */
-
-/*
-Lebih ringkas, bukan? Mungkin kode tersebut sulit dipahami, jadi mari kita bahas sedikit demi sedikit. Terdapat tiga bagian utama dalam sintaks for di atas:
-
-Pertama, variabel i sebagai index iterasi. Pada variabel ini kita menginisialisasi nilai awal dari perulangan.
-Kedua, operasi perbandingan. Pada bagian ini, JavaScript akan melakukan pengecekan kondisi apakah perulangan masih perlu dilakukan. Jika bernilai true, maka kode di dalam blok for akan dijalankan.
-Ketiga, increment/decrement. Di sini kita melakukan penambahan atau pengurangan variabel iterasi. Jadi, pada contoh di atas variabel i akan ditambah dengan 1 di setiap akhir perulangan. Perubahan nilai ini penting karena jika kita mengubah nilainya, proses perulangan dapat berjalan selamanya karena kondisi akan terus terpenuhi.
-JIka diartikan, maka kode di atas bisa dimaknai dengan “Jika i kurang dari 5, maka tampilkan nilai i.”
+Luke
+Han
+Chewbacca
+Leia
 */
