713a00986d7ae11d1e3cc856752e3d2fef94bbba
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 18:44:50 +0700

    Object-Oriented Programming (OOP): Object Composition.

--- index.js
+++ index.js
@@ -1,43 +1,98 @@
 // # Object-Oriented Programming (OOP)
 
-/* # Overriding Method
-  Hampir sama dengan overriding constructor, namun yang di-override di sini adalah method-method yang ada pada parent class. Pada dasarnya semua method yang ada pada kelas parent dapat diakses langsung di child kelasnya (as is).
-      super.methodName();
-Kadang kita tidak menggunakan sebuah method seutuhnya sama seperti parent kelasnya. namun dapat menambahkan perintah tertentu ataupun menguranginya. Berikut merupakan contoh override pada method sendMessage.
+/* # Object Composition
+  Object composition adalah prinsip komposisi dari sebuah alur bisnis tanpa perlu melakukan pewarisan dari parent-nya. Prinsip ini didasarkan di mana kita telah mendefinisikan kumpulan-kumpulan perilaku (method/function) sehingga ketika kita ingin membuat alur bisnis lain dengan beberapa perilaku (method) yang sama, kita dapat menggunakan yang sudah ada tanpa melakukan inheritance/pewarisan.
+
+  Pada dasarnya konsep yang harus dilakukan adalah:
 */
-class WhatsApp extends Mail {
-  constructor(username, isBussinessAccount, phone) {
-    super(phone);
-    this.username = username;
-    this.isBussinessAccount = true;
-  }
 
-  // Overriding method => Melakukan Override Total
-  sendMessage = function (msg, to) {
-    console.log('Send by WA');
-  }
+//  1.Memisahkan fungsi-fungsi umum yang biasa digunakan.
+const yourGenericAction = params => ({
+  actionA: () => { /** do action A **/ },
+  actionB: () => { /** do action B **/ },
+});
+
+//  2.Membuat Class seperti biasa.
+const YourClassName = (paramA, paramB) => {
+}
+
+//  3.Kita dapat menyimpan attribute yang kita punya ke dalam sebuah object, biasanya seorang engineer menggunakan konstanta dengan nama self atau state untuk menampung attribute.
+const YourClassName = (paramA, paramB) => {
+  const self = {
+    paramsA,
+    paramsB
+  };
+}
+
+//  4.Menambahkan perilaku (method/function) yang hanya ada pada kelas tersebut.
+const YourClassName = (paramA, paramB) => {
+  const self = {
+    paramsA,
+    paramsB
+  };
+
+  const yourSpecificActions = self => ({
+    specificActinA: { /** do action A **/ },
+  });
+}
+
+//  5.Membuat kumpulan attribute, generic method, dan spesific method menjadi satu objek.
+const YourClassName = (paramA, paramB) => {
+  const self = {
+    paramsA,
+    paramsB
+  };
+
+  const yourSpecificActions = self => ({
+    specificActinA: { /** do action A **/ },
+  });
+
+  return Object.assign(self, yourGenericAction(self), yourSpecificActions(self))
 }
 
 
-// Dari contoh tersebut, ketika kita memanggil method sendMessage hanya akan mengeksekusi kode yang ada pada child class.
-const wa1 = new WhatsApp('di', true, 089000999888);
-wa1.sendMessage('halo', 089000999888);
-/**
-Output:
-Send by WA
-**/
 
 
-// Untuk tetap melakukan eksekusi kode pada parent class maka perlu menggunakan operator super.methodName().
-sendMessage = function (msg, to) {
-  super.sendMessage(msg, to);
-  console.log('Send by WA');
-}
 
+// Sebagai contoh, dari hirarki Mail yang sudah kita buat sebelumnya. kita akan merombak dan membuatnya dengan pendekatan Object composition :.
+
+// [1] list of abstractions
+const canSendMessage = self => ({
+  sendMessage: () => console.log('send message:', self.message)
+});
+
+const checkIsValidPhone = self => ({
+  isValid: () => console.log('valid phone', self.from)
+});
+
+// [2] crate object composition
+const personalEnterprise = (from, message, store) => {
+  // [3] attributes
+  const self = {
+    from,
+    message,
+    store
+  };
+  // [4] method
+  const personalEnterpriseBehaviors = self => ({
+    createCatalog: () => console.log('Catalog has created: ', self.store)
+  });
+
+  // [5] create object composition
+  return Object.assign(self, personalEnterpriseBehaviors(self), canSendMessage(self), checkIsValidPhone(self));
+};
+
+const pe1 = personalEnterprise('pengirim@gmail.com', 'hei produk baru nih', 'Dicoding Store');
+pe1.createCatalog(); //Catalog has created:  Dicoding Store
+pe1.sendMessage(); //send message: hei produk baru nih
 
 /*
-Catatan:
-super(...) digunakan untuk memanggil constructor parent, dan hanya dapat digunakan di constructor.
+Penjabaran kode di atas:
+  - Kita membuat sebuah abstraksi untuk method-method yang umum digunakan (di sini misalkan method mengirim pesan, dan validasi nomor hp).
+  - Kita membuat sebuah kelas baru dengan nama personalEnterprise, di mana seperti biasa kita dapat menggunakan parameter yang akan digunakan.
+  - Pada  object composition ini, penggunaan parameter biasa digunakan untuk mendaftarkan attribute-attribute dari kelas tersebut. Pada contoh di atas, kita mengumpulkan attribute tersebut pada konstanta bernama self atau state.
+  - Method, kita dapat juga menambahkan method/fungsi yang spesifik hanya ada pada kelas tersebut (kapabilitasnya hanya pada kelas tersebut / tidak umum).
+  - Proses pembuatan object dengan perintah Object.assign(attribute, method1, method2, methodN).
 
-super.methodName(...) digunakan untuk memanggil parent method.
+Dari contoh kode di atas maka kita dapat membuat sebuah object dengan nama personalEnterprise tanpa harus melakukan pewarisan.
 */
