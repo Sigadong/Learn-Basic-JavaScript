75b78c0aa1f824a788ae4487225c6c4185545fe3
Sigadong <irwanto.regar30@gmail.com>
2021-04-11 10:06:39 +0700

    JavaScript Fundamentals: Operator -> Logical Operator.

--- index.js
+++ index.js
@@ -1,42 +1,36 @@
 // # JAVASCRIPT FUNDAMENTALS
 
-/* - Comparison Operator
-Terdapat serangkaian karakter khusus yang disebut dengan operator pembanding/komparasi yang dapat mengevaluasi dan membandingkan dua nilai. Berikut daftar operator dan fungsinya:
-Operator:      Fungsi:
-  ==         Membandingkan kedua nilai apakah sama. (tidak identik).
-  !=         Membandingkan kedua nilai apakah tidak sama. (tidak identik).
-  ===        Membandingkan kedua nilai apakah identik.
-  !==        Membandingkan kedua nilai apakah tidak identik.
-  >          Membandingkan dua nilai apakah nilai pertama lebih dari nilai kedua.
-  >=         Membandingkan dua nilai apakah nilai pertama lebih atau sama dengan nilai kedua.
-  <          Membandingkan dua nilai apakah nilai pertama kurang dari nilai kedua.
-  <=         Membandingkan dua nilai apakah nilai pertama kurang atau sama dengan nilai kedua.
+/* - Logical Operator.
+Terdapat beberapa operator lain yang dapat kita gunakan untuk menetapkan logika yang lebih kompleks, yakni dengan logical operators. Dengan logical operator, kita dapat menggunakan kombinasi dari dua nilai boolean atau bahkan lebih dalam menetapkan logika.
+
+Pada JavaScript terdapat tiga buah karakter khusus yang berfungsi sebagai logical operator. Berikut macam-macam logical operator dan fungsinya:
+
+Operator:	  Deskripsi:
+  !         Operator tidak (not). Digunakan untuk membalikkan suatu kondisi.
+  &&	      Operator dan (and). Logika akan menghasilkan nilai true apabila semua kondisi terpenuhi (bernilai true).
+  ||	      Operator atau (or). Logika akan menghasilkan nilai true apabila ada salah satu kondisi terpenuhi (bernilai true).
 */
 
 let a = 10;
 let b = 12;
 
-console.log(a < b);
-console.log(a > b);
+/* AND operator */
+console.log(a < 15 && b > 10); // (true && true) -> true
+console.log(a > 15 && b > 10); // (false && true) -> false
+
+/* OR operator */
+console.log(a < 15 || b > 10); // (true || true) -> true
+console.log(a > 15 || b > 10); // (false || true) -> true
+
+/* NOT operator */
+console.log(!(a < 15)); // !(true) -> false
+console.log(!(a < 15 && b > 10)); // !(true && true) -> !(true) -> false
 
 /* output
 true
 false
-*/
-
-/*Perbedaan antara “Sama” dan “Identik”.
-Dalam operator komparasi di JavaScript, hal yang menjadi sedikit “tricky” adalah membedakan antara “sama” (==) dan “identik” (===).
-
-Kita sudah mengetahui bahwa setiap nilai pasti memiliki tipe data baik itu number, string atau boolean. Contohnya sebuah string “10” dan number 10 merupakan hal yang serupa, tetapi keduanya tidak benar-benar sama.
-*/
-
-const aString = '10';
-const aNumber = 10
-
-console.log(aString == aNumber) // true, karena nilainya sama-sama 10
-console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
-
-/* output
 true
+true
+false
 false
 */
